b6302389bc79783e83ef375e30e00d4fe39ea6a9
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-06-21 18:30:39 +0200

    gg

---
+++ clases/servivo.py
@@ -0,0 +1,11 @@
+class ser_vivo:
+    def __init__(self,energia):
+        self.energia=energia
+    def is_vivo(self):
+        return self.energia>0
+    def is_muerto(self):
+        self.energia=0
+    def get_energia(self):
+        return self.energia
+    def set_energia(self,x):
+        self.energia=x
--- lanzador.py
+++ lanzador.py
@@ -1,3 +1,6 @@
 from clases.movimientos import movimiento_general,tipo_movimiento,movimiento_especifico
+from clases.escenarios import Escenario
 def main():
     patada=movimiento_general("patada",tipo_movimiento.ataque,80)
+    escenario=Escenario.denombre("torre_stark")
+    print(escenario.__str__())
